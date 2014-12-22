6f174802dd77e3fb68ceb6d55ea40f1b78e27cf3
chris.schluetter@gmail.com <chris.schluetter@gmail.com@00cea1ae-f520-6794-c2bc-6dd775f73025>
2014-12-22 13:34:30 +0000

    function.cpp hinzugefügt

--- backend/context.hpp
+++ backend/context.hpp
@@ -3,13 +3,20 @@
 #include <string>
 #include <stack>
 #include "object.hpp"
+#include "vm.hpp"
 
 class Context
 {
 private:
     std::stack<Object> m_stack;
+    VM m_vm;
 
 public:
+    Context(VM const& vm)
+        :m_vm(vm)
+    {
+    }
+
     void pushInteger(int value);
     void pushString(std::string value);
     void call(std::string name, int argCount);
