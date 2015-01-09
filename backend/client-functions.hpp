edc38fce880fdc1f69b7f19ebaca2406713885b7
paus.fabian@gmail.com <paus.fabian@gmail.com@00cea1ae-f520-6794-c2bc-6dd775f73025>
2015-01-09 14:59:24 +0000

    Abfrage der Client-Daten in beliebegem Thread

--- backend/client-functions.hpp
+++ backend/client-functions.hpp
@@ -3,6 +3,8 @@
 
 #include "function.hpp"
 
+#include "client.hpp"
+
 class StartClient : Function
 {
 public:
@@ -18,6 +20,44 @@ public:
         String* id = dynamic_cast<String*>(args[0].get());
 
         vm.startClient(id->getValue());
+    }
+};
+
+class SetClientId : Function
+{
+public:
+    SetClientId() : Function("setClientId")
+    {
+    }
+
+    void execute(VM& vm, arg_list const& args) const override
+    {
+        if (args.size() != 1)
+            throw std::runtime_error("Argument count not valid");
+
+        String* id = dynamic_cast<String*>(args[0].get());
+
+        Client& client = vm.getCurrentClient();
+        client.clientId = id->getValue();
+    }
+};
+
+class SetPosition : Function
+{
+public:
+    SetPosition() : Function("setPosition")
+    {
+    }
+
+    void execute(VM& vm, arg_list const& args) const override
+    {
+        if (args.size() != 1)
+            throw std::runtime_error("Argument count not valid");
+
+        String* pos = dynamic_cast<String*>(args[0].get());
+
+        Client& client = vm.getCurrentClient();
+        client.position = pos->getValue();
     }
 };
 
